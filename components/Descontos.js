863914eab904552b31e2f025e8866c4128ca6d33
Helder Fonseca <helder2391@gmail.com>
2022-11-14 15:32:18 -0100

    translate to swaili

--- components/Descontos.js
+++ components/Descontos.js
@@ -3,9 +3,9 @@ const Descontos = () => {
 
     return (
         <div className="pt-28 mx-auto text-center">
-            <h2 className="text-5xl pb-3 font-bold">Get discounts and become different!</h2> 
-            <p className="font-medium p-3"> Online requires the search for new strategies and models </p>
-            <p className="font-medium pt-0.5 pb-0.5 pr-3 pl-3 bg-[#FBED04] inline rounded-md"> Choose your best plan for your company! </p> 
+            <h2 className="text-5xl pb-3 font-bold"> Wasiliana nasi leo, na uwe tofauti! </h2> 
+            <p className="font-medium p-3"> Mkondoni unahitaji utaftaji wa mikakati na miundo mipya </p>
+            <p className="font-medium pt-0.5 pb-0.5 pr-3 pl-3 bg-[#FBED04] inline rounded-md"> Chagua mpango bora wa kampuni yako! </p> 
         </div>
     )
 
--- components/Top.js
+++ components/Top.js
@@ -8,19 +8,18 @@ const Top = () => {
         <div className="mx-auto max-w-6xl">
             <div className="flex md:flex-row flex-col-reverse md:gap-16 gap-8 text-[#f8f8ff]">
                 <div className="md:w-1/2 w-full md:mt-20">
-                    <h1 className="text-4xl md:w-[25rem]"><strong>A Partner that helps your business compete on digital channels</strong></h1> 
-                    <p className="mt-4 mb-4 md:w-96 text-3xl font-light italic">
-                       A tidy house is well-thought-out! <span className="font-bold"> Enjoy our exclusive Website deals!</span>
+                    <h1 className="text-4xl md:w-[28rem]"><strong>Mshirika anayesaidia biashara yako kushindana kwenye chaneli za kidijitali</strong></h1> 
+                    <p className="mt-4 mb-4 md:w-[28rem] text-3xl font-light italic">
+                       Nyumba nzuri inafikiriwa vizuri! <span className="font-bold">Furahia ofa zetu za kipekee za Tovuti!</span>
                     </p>
-                    <p className="mt-4 mb-4 md:w-96 text-justify font-medium">
-                    To keep up with audience, more and more companies are betting on the creation of digital customer relationship channels.
+                    <p className="mt-4 mb-4 md:w-[28rem] text-justify font-medium">
+                        Ili kuendelea na watazamaji, makampuni mengi zaidi yanaweka kamari kuhusu uundaji wa njia za kidijitali za uhusiano na wateja.
                     </p>
-                    <p className="mb-4 md:w-96 text-justify font-medium" >
-                        The online presence is much more than having a website on the internet:
-                        it is necessary to build authority and credibility of a company, becoming a reference in your area.
+                    <p className="mb-4 md:w-[28rem] text-justify font-medium" >
+                        Uwepo wa mtandaoni ni zaidi ya kuwa na tovuti kwenye mtandao: ni muhimu kujenga mamlaka na uaminifu wa kampuni, kuwa kumbukumbu katika eneo lako.
                     </p>
-                    <p className="mb-4 md:w-96 text-justify font-medium" >
-                        Yellow Pages can help you create your Website through packages that meet your needs.
+                    <p className="mb-4 md:w-[28rem] text-justify font-medium" >
+                        Yellow Pages itakusaidia kuunda Tovuti yako kupitia vifurushi vinavyokidhi mahitaji yako.
                     </p>
                 </div>
                 
