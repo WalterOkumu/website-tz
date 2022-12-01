7c0a6c5d67ff95e049b28706454288d509e207e0
Helder Fonseca <helder2391@gmail.com>
2022-12-01 12:46:18 -0100

    add phone field

--- components/ContactForm.js
+++ components/ContactForm.js
@@ -29,15 +29,20 @@ export default function ContactForm() {
                 <div className="ml-form-embedContent" style={{marginBottom:0}}></div>
                 <form className="ml-block-form" action="https://static.mailerlite.com/webforms/submit/h4u1n5" data-code="h4u1n5" method="post" target="_blank">
                 <div className="ml-form-formContent">
-                    <div className="ml-form-fieldRow">
-                    <div className="ml-field-group ml-field-name">
-                        <input aria-label="name" type="text" className="form-control" data-inputmask="" name="fields[name]" placeholder="Jina" autoComplete="name" />
-                    </div>
+                    <div class="ml-form-fieldRow">
+                        <div class="ml-field-group ml-field-name ml-validate-required">
+                            <input aria-label="name" aria-required="true" type="text" class="form-control" data-inputmask="" name="fields[name]" placeholder="Jina" autocomplete="name" />
+                        </div>
                     </div>
                     <div className="ml-form-fieldRow">
                     <div className="ml-field-group ml-field-email ml-validate-email ml-validate-required">
                         <input aria-label="email" aria-required="true" type="email" className="form-control" data-inputmask="" name="fields[email]" placeholder="Barua pepe" autoComplete="email" />
                     </div>
+                    </div>
+                    <div class="ml-form-fieldRow">
+                        <div class="ml-field-group ml-field-phone ml-validate-required">
+                            <input aria-label="phone" aria-required="true" type="text" class="form-control" data-inputmask="" name="fields[phone]" placeholder="Simu" autocomplete="" />
+                        </div>
                     </div>
                     <div className="ml-form-fieldRow ml-last-item">
                     <div className="ml-field-group ml-field-company">
