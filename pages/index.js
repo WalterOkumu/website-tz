5e5711427c99a9619ed7adf7fc924b13125948b5
Helder Fonseca <helder2391@gmail.com>
2022-12-14 15:13:20 -0100

    add about info and footer

--- pages/index.js
+++ pages/index.js
@@ -3,20 +3,23 @@ import Image from 'next/image'
 import Descontos from '../components/Descontos'
 import Form from '../components/Form'
 import Header from '../components/Header'
-import Testemunhos from '../components/Testemunhos'
+import About from '../components/About'
 import Top from '../components/Top'
 import Script from 'next/script'
 import ContactForm from '../components/ContactForm'
+import Footer from '../components/Footer'
 
 export default function Home() {
   return (
-    <div className='pb-10'>
+    <div>
         <div className="pt-8 md:h-screen px-10 bg-no-repeat bg-cover bg-center bg-[url('/background-hero-mobile.png')] xl:bg-[url('/background-hero.png')]">
           <Header />
           <Top />
         </div>
+        <About />
         <Descontos />
         <ContactForm />
+        <Footer />
         {/*<Form></Form>
         <Testemunhos></Testemunhos>*/}
     </div>
